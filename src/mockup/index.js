7897f75cbd5b394dc700b35327905a088032b8b8
nsexpert <enjoygoldlife@gmail.com>
2023-01-28 03:38:57 +0900

    chore:create mockup data

---
+++ src/mockup/index.js
@@ -0,0 +1,39 @@
+const MockUpData = [
+  {
+    id: 1435,
+    name: 'qqqqqq',
+    contact: 123565634,
+    email: 'qqq@gmail.com',
+    imgSrc: 1
+  },
+  {
+    id: 22345234,
+    name: 'aaaaaa',
+    contact: 123123923,
+    email: 'aaa@gmail.com',
+    imgSrc: 2
+  },
+  {
+    id: 323452345,
+    name: 'bbbbbb',
+    contact: 123124123,
+    email: 'bbb@gmail.com',
+    imgSrc: 3
+  },
+  {
+    id: 42345435,
+    name: 'cccccc',
+    contact: 123123123,
+    email: 'ccc@gmail.com',
+    imgSrc: 4
+  },
+  {
+    id: 52345345,
+    name: 'dddddd',
+    contact: 123423123,
+    email: 'ddd@gmail.com',
+    imgSrc: 5
+  },
+];
+
+export default MockUpData;
